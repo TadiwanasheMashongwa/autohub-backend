d8fbfee330fbeaad2d9294e8d25acab87d2c70f8
Tadiwanashe Mashongwa <sbacohort@gmail.com>
2026-01-05 18:26:51 +0100

    fix: add email field to RegisterRequest DTO for customer registration

--- api/api/src/main/java/com/autohub/api/auth/RegisterRequest.java
+++ api/api/src/main/java/com/autohub/api/auth/RegisterRequest.java
@@ -5,6 +5,7 @@ public class RegisterRequest {
     private String password;
     private String firstName;
     private String lastName;
+    private String email; // Added this field
     private String phoneNumber;
     private String businessName;
     private String address;
@@ -13,13 +14,10 @@ public class RegisterRequest {
 
     // GETTERS
     public String getUsername() { return username; }
-
-    // Alias to fix the compilation error in the controller
-    public String getEmail() { return username; }
-
     public String getPassword() { return password; }
     public String getFirstName() { return firstName; }
     public String getLastName() { return lastName; }
+    public String getEmail() { return email; } // Updated to return actual email
     public String getPhoneNumber() { return phoneNumber; }
     public String getBusinessName() { return businessName; }
     public String getAddress() { return address; }
@@ -29,6 +27,7 @@ public class RegisterRequest {
     public void setPassword(String password) { this.password = password; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; } // Added this setter
     public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
     public void setBusinessName(String businessName) { this.businessName = businessName; }
     public void setAddress(String address) { this.address = address; }
